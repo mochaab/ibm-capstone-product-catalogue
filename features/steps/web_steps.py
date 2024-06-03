6f0930e5f6b9b534f4e010c6f4c86c38203dd908
mochaab <charissamorales.bscs@gmail.com>
2024-06-03 14:31:28 -0400

    verify that a specific message is present

--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -112,6 +112,23 @@ def step_imp(context, button):
     # Use context.driver.find_element_by_id(button_id) line to find the button element on the web page based on the generated button_id and call the click() method 
     context.driver.find_element_by_id(button_id).click()
 
+#  And I should not see "Sheets" in the results
+@then('I should not see "{name}" in the results')
+def step_impl(context, name):
+    element = context.driver.find_element_by_id('search_results')
+    assert(name not in element.text)
+
+@then('I should see the message "{message}"')
+def step_impl(context, message):
+    # Use WebDriverWait to wait for the specified message to be present in the element with the ID 'flash_message'
+    # Use the expected_conditions.text_to_be_present_in_element method to check if the provided message is present in the text content of the element.
+    element = WebDriverWait(context.driver, context.wait_seconds).until(
+       expected_conditions.text_to_be_present_in_element((By.ID, 'flash_message'))
+    )
+    # Use the assert(found) statement to verify that the message was found in the flash message area.
+    assert(element)
+    
+
 ##################################################################
 # This code works because of the following naming convention:
 # The id field for text input in the html is the element name
